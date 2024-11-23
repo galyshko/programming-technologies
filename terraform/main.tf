b08f4190952d9a96bbbd0f010e6690db7d3bec23
galyshko <serjhalj@gmail.com>
2024-11-23 12:51:59 +0200

    11

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,7 +47,7 @@ resource "aws_security_group" "web_app" {
 
 resource "aws_instance" "webapp_instance" {
   ami           = "ami-05edb7c94b324f73c"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.web_app.id]
 
 
