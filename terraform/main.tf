190bdee44a60c7399d0965bba65ee97ec996908d
galyshko <serjhalj@gmail.com>
2024-11-23 15:08:27 +0200

    25

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,12 @@ terraform {
       version = "~> 5.0"
     }
   }
-
+backend "s3" {
+    bucket         = "galyshko-tf-state"
+    key            = "terraform.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "galyshko-tf-lockid"
+  }
 
 }
 
@@ -16,7 +21,7 @@ provider "aws" {
 }
 
 resource "aws_security_group" "web_app" {
-  name        = "web_app"
+  name        = "web_app_unique"
   description = "security group for web application"
 
   ingress {
@@ -47,8 +52,8 @@ resource "aws_security_group" "web_app" {
 
 resource "aws_instance" "webapp_instance" {
   ami           = "ami-05edb7c94b324f73c"
-    instance_type = "t2.micro"
-  security_groups= ["web_app"]
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.web_app.id]
 
   tags = {
     Name = "webapp_instance"
