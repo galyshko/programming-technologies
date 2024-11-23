c9ac008078f04fcbb25e84e54256e4a29a93e998
galyshko <serjhalj@gmail.com>
2024-11-23 12:15:25 +0200

    2

---
+++ main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+
+# Configure the AWS provider
+provider "aws" {
+  region = "eu-north-1a"
+}
+
+resource "aws_security_group" "web_app" {
+  name        = "web_app_unique"
+  description = "security group for web application"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web_app"
+  }
+}
+
+resource "aws_instance" "webapp_instance" {
+  ami           = "ami-05edb7c94b324f73c"
+  instance_type = "t2.micro"
+  security_group=["web_app"]
+  tags = {
+    Name = "webapp_instance"
+  }
+}
+
+output "instance_public_ip" {
+  value     = aws_instance.webapp_instance.public_ip
+  sensitive = true
+}
